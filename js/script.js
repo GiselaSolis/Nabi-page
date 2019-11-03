4c3e3024a5fb356384265ddcc90bbc4dd1b2d4f6
unknown <gisela.solis.roman@gmail.com>
2019-11-03 12:09:25 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var genero 
+
+function myFunctionNina (){
+    document.getElementById("nina").innerHTML = "<img src='./img/check.png'>";
+    document.getElementById("nina").style.textAlign = "center";
+}
+
+function myFunctionNino (){
+    document.getElementById("nino").innerHTML = "<img src='./img/check.png'>";
+    document.getElementById("nino").style.textAlign = "center";
+}
